9790df88c14320c919049495edde29d7fe08d359
Emrullah Cirit <emrullahcirit20@gmail.com>
2024-01-25 03:06:51 +0300

    fix indent

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ module "network" {
 }
 
 module "instances" {
-	source = "./modules/instance"
+  source = "./modules/instance"
 
   security_group_id = module.network.security_group
   subnet_ids = module.network.subnet_values
 }
 
 module "eks-cluster" {
-	source = "./modules/eks"
+  source = "./modules/eks"
 
   vpc_id = module.network.main-vpc
   subnet_ids = module.network.subnet_values
