e25bec8e68b50edd0edae644ad5aa0255316c927
Emrullah Cirit <emrullahcirit20@gmail.com>
2024-01-26 11:53:13 +0300

    Update instance.tf

--- modules/instance/instance.tf
+++ modules/instance/instance.tf
@@ -10,18 +10,18 @@ resource "aws_launch_configuration" "apache_server" {
 }
 
 resource "aws_autoscaling_group" "devops-case-asg" {
-  desired_capacity     = 2
-  max_size             = 2
-  min_size             = 2
-  health_check_type    = "EC2"
+  desired_capacity = 2
+  max_size = 2
+  min_size = 2
+  health_check_type = "EC2"
   health_check_grace_period = 300
 
   launch_configuration = aws_launch_configuration.apache_server.id
-  vpc_zone_identifier  = tolist(var.subnet_ids)
+  vpc_zone_identifier = tolist(var.subnet_ids)
 
   tag {
-    key                 = "Name"
-    value               = "case-httpd-instance"
+    key = "Name"
+    value = "case-httpd-instance"
     propagate_at_launch = true
   }
 }
